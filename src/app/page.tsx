a3f84f0755a8ae48f027c34d68e48a786b4f1efe
MKvaratshelia <kvaratsheliamihail@yandex.ru>
2023-11-09 20:30:33 +0300

    update Button component, add svgr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
 import { Htag } from '@/components';
+import { Button } from '@/components';
 
 export default function Home() {
-    return <Htag tag='h1'>Text</Htag>;
+    return (
+        <>
+            <Htag tag='h1'>Text</Htag>
+            <Button appearance='primary' arrow='right'>
+                кнопка
+            </Button>
+            <Button appearance='ghost' arrow='down'>
+                кнопка
+            </Button>
+        </>
+    );
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,36 @@
-import { ReactNode } from 'react';
+import { ReactNode, useMemo } from 'react';
+import styles from './Button.module.css';
+import { ButtonProps } from './Button.props';
+import classNames from 'classnames';
+import ArrowIcon from './arrow.svg';
 
-export const Button = (): ReactNode => {
-    return <button></button>;
+export const Button = ({
+    appearance,
+    arrow = 'none',
+    children,
+    className,
+    ...props
+}: ButtonProps): ReactNode => {
+    const classes = useMemo(
+        () => ({
+            [styles.primary]: appearance === 'primary',
+            [styles.ghost]: appearance === 'ghost',
+        }),
+        [appearance],
+    );
+
+    return (
+        <button className={classNames(styles.button, classes, className)} {...props}>
+            {children}
+            {arrow !== 'none' && (
+                <span
+                    className={classNames(styles.arrow, {
+                        [styles.right]: arrow === 'right',
+                        [styles.down]: arrow === 'down',
+                    })}>
+                    <ArrowIcon />
+                </span>
+            )}
+        </button>
+    );
 };
